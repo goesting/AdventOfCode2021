9634cd80d4a200d69e361279f87d326a7daf7f8e
goesting <75796795+goesting@users.noreply.github.com>
2021-12-03 17:20:21 +0100

    Update day2_vba.vb
    cleaned tabs

--- day2_vba.vb
+++ day2_vba.vb
@@ -4,20 +4,19 @@ f = 0
 d = 0
 
 For i = 1 To lastrow
-inp = Cells(i, 1).Value
-inp_s = Split(inp, " ")
-c = inp_s(0)
-a = inp_s(1)
-
-If c = "forward" Then f = f + a
-End If
-If c = "down" Then d = d + a
-If c = "up" Then d = d - a
+  inp = Cells(i, 1).Value
+  inp_s = Split(inp, " ")
+  c = inp_s(0)
+  a = inp_s(1)
+  If c = "forward" Then f = f + a
+  If c = "down" Then d = d + a
+  If c = "up" Then d = d - a
 Next i
 result = f * d
 Debug.Print result
-
 End Sub
+
+
 Sub day2b()
 lastrow = ActiveSheet.Range("A" & Rows.Count).End(xlUp).Row
 f = 0
@@ -25,17 +24,17 @@ d = 0
 aim = 0
 
 For i = 1 To lastrow
-inp = Cells(i, 1).Value
-inp_s = Split(inp, " ")
-c = inp_s(0)
-a = inp_s(1)
+  inp = Cells(i, 1).Value
+  inp_s = Split(inp, " ")
+  c = inp_s(0)
+  a = inp_s(1)
 
-If c = "forward" Then
-f = f + a
-d = d + (aim * a)
-End If
-If c = "down" Then aim = aim + a
-If c = "up" Then aim = aim - a
+  If c = "forward" Then
+    f = f + a
+    d = d + (aim * a)
+  End If
+  If c = "down" Then aim = aim + a
+  If c = "up" Then aim = aim - a
 
 Next i
 result = f * d
