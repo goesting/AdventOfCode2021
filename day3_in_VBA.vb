49478f101b05cac1d6e713f4ffd1129b0d9a7869
goesting <75796795+goesting@users.noreply.github.com>
2021-12-03 16:18:14 +0100

    Update day3_in_VBA.vb

--- day3_in_VBA.vb
+++ day3_in_VBA.vb
@@ -87,70 +87,15 @@ For k = 1 To 12
 Next k
 miss_sol_Dec = Bintodec(miss_solution)
 
-
-
 result = miss_sol_Dec * match_sol_dec
 
 Debug.Print result
 
-
-
-
-
-
 End Sub
-Sub trqch()
 
-    common_str = ""
-    uncomm_str = ""
-For i = LBound(common) To UBound(common)
-    If common(i) > 0 Then
-        common_str = "1" & common_str
-        uncomm_str = "0" & uncomm_str
-    Else
-        common_str = "0" & common_str
-        uncomm_str = "1" & uncomm_str
-    End If
-
-
-
-Next i
-x = 1
-
-lett = 1
-Do While matchdict.Count > 1
-lett_to_match = Mid(common_str, lett, 1)
-For Each key In matchdict.Keys
-    matchee = Mid(matchdict(key), lett, 1)
-    If matchee <> lett_to_match Then
-        matchdict.Remove key
-    End If
-    
-Next key
-lett = lett + 1
-Loop
-
-lett = 1
-Do While misshdict.Count > 1
-lett_to_match = Mid(uncomm_str, lett, 1)
-For Each key In missdict.Keys
-    matchee = Mid(matchdict(key), lett, 1)
-    If matchee <> lett_to_match Then
-        missdict.Remove key
-    End If
-    
-Next key
-lett = lett + 1
-Loop
-x = 1
-
-
-
-
-End Sub
 Function Bintodec(bin As String) As LongLong
-For i = 1 To Len(bin)
-    Bintodec = Bintodec + WorksheetFunction.Power(2, Len(bin) - i) * Mid(bin, i, 1)
-Next i
+    For i = 1 To Len(bin)
+        Bintodec = Bintodec + WorksheetFunction.Power(2, Len(bin) - i) * Mid(bin, i, 1)
+    Next i
 
 End Function
