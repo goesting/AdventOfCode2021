011d696a6cd649ac6fa2d8c799efecc70302d3db
goesting <75796795+goesting@users.noreply.github.com>
2021-12-03 16:53:03 +0100

    Update day3_in_VBA.vb
    added part a

--- day3_in_VBA.vb
+++ day3_in_VBA.vb
@@ -1,11 +1,71 @@
-Sub day4b()
+Sub day3a()
+Dim secondsSince As Single
+secondsSince = Timer()
+lastrow = ActiveSheet.Range("A" & Rows.Count).End(xlUp).Row
+Dim arr() As Variant
+ReDim arr(1 To lastrow)
+'Dim d As Object
+'Set d = CreateObject("Scripting.Dictionary")
+'inp = WorksheetFunction.Transpose(Range("A1:A1097"))
+
+Dim coun() As Variant
+ReDim coun(1 To 12)
+
+For i = 1 To lastrow
+inp = Cells(i, 1).Value
+arr(i) = inp
+Next i
+
+For i = LBound(arr) To UBound(arr)
+For j = 1 To Len(arr(i))
+c = Mid(arr(i), j, 1)
+If c = "1" Then
+    coun(j) = coun(j) + 1
+Else
+    coun(j) = coun(j) - 1
+End If
+
+
+
+
+Next j
+Next i
+
+
+Gamma = 0
+epsilon = 0
+
+For k = 1 To 12
+most_nr = coun(13 - k)
+If most_nr > 0 Then
+most_nr = 1
+Gamma = Gamma + WorksheetFunction.Power(2, (k - 1))
+Else
+most_nr = 0
+epsilon = epsilon + WorksheetFunction.Power(2, (k - 1))
+End If
+
+
+
+
+Next k
+result = epsilon * Gamma
+Debug.Print "solution for part a: " & result
+Debug.Print "time for part a: " & Timer - secondsSince
+
+
+End Sub
+Sub day3b()
 lastrow = ActiveSheet.Range("A" & Rows.Count).End(xlUp).Row
 Dim common() As Variant
+Dim secondsSince As Single
 ReDim common(1 To 12)
 Dim matchdict As Object
 Set matchdict = CreateObject("Scripting.Dictionary")
 Dim missdict As Object
 Set missdict = CreateObject("Scripting.Dictionary")
+secondsSince = Timer()
+
 For i = 1 To lastrow
     inp = ActiveSheet.Cells(i, 1).Value
     matchdict.Add i, inp
@@ -87,15 +147,15 @@ For k = 1 To 12
 Next k
 miss_sol_Dec = Bintodec(miss_solution)
 
+
+
 result = miss_sol_Dec * match_sol_dec
 
-Debug.Print result
+Debug.Print "solution for part b: " & result
+Debug.Print "time for part b: " & Timer - secondsSince
+
+
+
+
 
 End Sub
-
-Function Bintodec(bin As String) As LongLong
-    For i = 1 To Len(bin)
-        Bintodec = Bintodec + WorksheetFunction.Power(2, Len(bin) - i) * Mid(bin, i, 1)
-    Next i
-
-End Function
