5065860ae13853ad611a506264c2118c01de5e1b
goesting <75796795+goesting@users.noreply.github.com>
2021-12-12 18:06:11 +0100

    Update day12_vba.vb

--- vba/day12_vba.vb
+++ vba/day12_vba.vb
@@ -1,5 +1,5 @@
 'ROUGH DRAFT, NEEDS CLEANUP AND OPTIMALIZATION
-
+'0.1 bit of cleanup done, but till slow
 
 'Timing Functions
 Private Declare PtrSafe Function getFrequency Lib "kernel32" Alias "QueryPerformanceFrequency" (ByRef Frequency As Currency) As Long
@@ -38,63 +38,42 @@ Debug.Print ""
 '----------------------------------------------------------
 End Sub
 Function getNextNode(cavedata, currentpath, ByRef result, Optional part2 = False, Optional ByVal twicevisitedsmall = False)
-On Error GoTo errhand
-'If part2 Then twicevisitedsmall = False
-
-
 currentnode = Split(currentpath, "-")(UBound(Split(currentpath, "-")))
 If currentnode = "end" Then
     'path done
     getNextNode = currenpath
-    'Debug.Print currentpath
-    DoEvents
+    DoEvents 'prevent excel from being unresponsive
     result = result + 1
 Else
 'For all possible next nodes
-For i = LBound(cavedata, 1) To UBound(cavedata, 1)
-    For j = 0 To 1
-    If (cavedata(i, j) = currentnode) Then
-        NextNode = cavedata(i, 1 - j)
-        If NextNode = "start" Then
-        ElseIf Not NextNode = UCase(NextNode) Then
-        
-            If part2 And Not twicevisitedsmall Then
-                If Len(currentpath) < Len(Replace(currentpath, NextNode, "")) + Len(NextNode) * 2 Then
-                    If Len(currentpath) > Len(Replace(currentpath, NextNode, "")) Then
-                    getNextNode = getNextNode(cavedata, currentpath & "-" & NextNode, result, part2, True)
-                    Else
-                    getNextNode = getNextNode(cavedata, currentpath & "-" & NextNode, result, part2, twicevisitedsmall)
+    For i = LBound(cavedata, 1) To UBound(cavedata, 1)          'check all possible paths for ones matching thisnode
+        For j = 0 To 1                                          'nodes can go left->right as right -> left, so check both
+            If (cavedata(i, j) = currentnode) Then              'current node found in data
+                NextNode = cavedata(i, 1 - j)                   'nextnode is the other one in the list
+                If NextNode = "start" Then                      'if it connects to start, ignore
+                ElseIf Not NextNode = UCase(NextNode) Then      'if lowercase, do checks for times visited etc
+                    If part2 And Not twicevisitedsmall Then                                                                         'no small cave has been visited twice
+                        If Len(currentpath) < Len(Replace(currentpath, NextNode, "")) + Len(NextNode) * 2 Then                      'if visited small cave 0 or 1 times
+                            If Len(currentpath) > Len(Replace(currentpath, NextNode, "")) Then                                      'if its 1 times, do the double visit and set to true
+                                getNextNode = getNextNode(cavedata, currentpath & "-" & NextNode, result, part2, True)
+                            Else                                                                                                    'else its an unvisited node, so go for it
+                                getNextNode = getNextNode(cavedata, currentpath & "-" & NextNode, result, part2, twicevisitedsmall)
+                            End If
+                        End If
+                    ElseIf Len(currentpath) = Len(Replace(currentpath, NextNode, "")) Then                                          'either part 1, or part 2 but we have already visited a small thing twice
+                        getNextNode = getNextNode(cavedata, currentpath & "-" & NextNode, result, part2, twicevisitedsmall)         'do next only if small cave wasnt visited before, by checking if it exists in currentpath
                     End If
+                Else                                                                                                                'if uppercase, just do next, always allowed
+                    getNextNode = getNextNode(cavedata, currentpath & "-" & NextNode, result, part2, twicevisitedsmall)
                 End If
-                
-        
-            ElseIf Len(currentpath) = Len(Replace(currentpath, NextNode, "")) Then
-            
-                getNextNode = getNextNode(cavedata, currentpath & "-" & NextNode, result, part2, twicevisitedsmall)
             End If
-        
-        Else
-        
-        getNextNode = getNextNode(cavedata, currentpath & "-" & NextNode, result, part2, twicevisitedsmall)
-        End If
-    End If
-    Next j
-Next i
-'next node
+        Next j
+    Next i
 End If
-
-GoTo contin
-errhand:
-Stop
-Resume
-contin:
 
 End Function
 
-
-
 Sub day12b()
-On Error GoTo errhand
 'variables for timing
    Dim startTime As Currency
    Dim endTime As Currency
@@ -120,11 +99,8 @@ Path = getNextNode(grid, currenpath, result, True)
 '------ Output results and time taken ---------------------
 getTime endTime
 timeElapsed = (endTime - startTime) / perSecond
-Debug.Print "Solution to part 1: " & result
+Debug.Print "Solution to part 2: " & result
 Debug.Print "Time taken = " & timeElapsed & " seconds"
 Debug.Print ""
 '----------------------------------------------------------
-errhand:
-Stop
-Resume
 End Sub
